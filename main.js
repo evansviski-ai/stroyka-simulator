ee73d1f88be869b4eafa15c8af40a814bb7c6267
evansviski-ai <evansviski@gmail.com>
2026-06-19 16:15:06 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.module.js";
+import * as THREE from "https://esm.sh/three@0.160.0";
 
-import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.160.0/examples/jsm/controls/OrbitControls.js";
+import { OrbitControls } from "https://esm.sh/three@0.160.0/examples/jsm/controls/OrbitControls.js";
+
 
 const STORAGE_KEY = "stroyka-bs-world";
 
